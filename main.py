36d987ac0efc3d31d6ae4b9f639bc8566a9668db
aniket12341 <aniketm12341@gmail.com>
2020-12-27 20:53:28 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+while True:
+    pas = input("Enter your password: ")
+    if pas != 'aniket':
+        print("Wrong Password! Try Again")
+    else:
+        print("Entry Successful")
+        break
